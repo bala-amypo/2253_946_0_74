df9f071ed3550368f35f154a606b36bbda015463
bala-amypo <bala@amypo.in>
2025-12-17 08:28:41 +0000

    User commit

--- src/main/java/com/example/demo/service/Api_implementation/Api_SerImplementation.java
+++ src/main/java/com/example/demo/service/Api_implementation/Api_SerImplementation.java
@@ -5,8 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.example.demo.entity.Student;
-import com.example.demo.repository.StudentRepository;
+import com.example.demo.entity.Api;
+import com.example.demo.repository.Api_Repository;
 
 @service
 public class Api_SerImplementation implements ApiService {
