b184d5f0f4e39f57339ce169f98eebfa2e10b7a8
bala-amypo <bala@amypo.in>
2025-12-17 06:13:21 +0000

    User commit

--- src/main/java/com/example/demo/service/serviceImplementation/Api_service.java
+++ src/main/java/com/example/demo/service/serviceImplementation/Api_service.java
@@ -1,4 +1,7 @@
 packagecom.example.demo.service;
 import java.util.List;
 import com.example.demo.entity.Student;
-public interface Api
+public interface Api_service {
+  public  Api createData(Api a);
+   List<Api> fetchRecord();
+}
