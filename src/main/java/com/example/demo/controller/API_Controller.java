5b0f60a73846a7b401b79ce16cd492163070a9a6
bala-amypo <bala@amypo.in>
2025-12-17 06:02:32 +0000

    User commit

--- src/main/java/com/example/demo/controller/API_Controller.java
+++ src/main/java/com/example/demo/controller/API_Controller.java
@@ -1,1 +1,20 @@
 package com.example.demo.controller;
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.Student;
+import com.example.demo.service.StudentService;
+
+@RestController
+public class API_Controller{
+    @Autowired
+    API_Controller api;
+    
+}
